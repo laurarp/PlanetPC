3030a29e85e8075b01b20beaf657d5a75f0f3499
Guillermo <guillo5508@hotmail.com>
2017-02-20 21:19:51 -0500

    constructor de herencia

--- src/AuxiliarAlmacenamiento.java
+++ src/AuxiliarAlmacenamiento.java
@@ -1,6 +1,10 @@
 
 public class AuxiliarAlmacenamiento extends Actor{
 	
+	public AuxiliarAlmacenamiento(String nombre, String id, String contrasena) {
+		super(nombre, id, contrasena);
+		// TODO Auto-generated constructor stub
+	}
 	public void ingresarProductoCatalogo(ListaDescProducto listaDescProductos,DescripcionProducto descProducto)
 	{
 		
