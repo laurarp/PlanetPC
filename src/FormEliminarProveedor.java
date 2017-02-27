659e61afd223a64ae2bb3888be1f81cdc6d06f36
switowski143 <alejandro.mery@hotmail.com>
2017-02-27 01:23:08 -0500

    elimn

--- src/FormEliminarProveedor.java
+++ src/FormEliminarProveedor.java
@@ -2,6 +2,7 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
@@ -11,28 +12,20 @@ public class FormEliminarProveedor {
 
 	private JFrame frmEliminarProveedor;
 	private JTextField textField;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					FormEliminarProveedor window = new FormEliminarProveedor();
-					window.frmEliminarProveedor.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+	private Administrador Admin;
 
 	/**
 	 * Create the application.
 	 */
-	public FormEliminarProveedor() {
-		initialize();
+	public FormEliminarProveedor(Actor actor) {
+		try 
+		{
+			Admin = new Administrador(actor.getId(),actor.getNombre(),actor.getContrasena());
+			initialize();
+		} 
+		catch (Exception e) {
+			JOptionPane.showMessageDialog(null, e.getMessage());
+		}
 	}
 
 	/**
@@ -42,7 +35,7 @@ public class FormEliminarProveedor {
 		frmEliminarProveedor = new JFrame();
 		frmEliminarProveedor.setTitle("Eliminar proveedor");
 		frmEliminarProveedor.setBounds(100, 100, 450, 300);
-		frmEliminarProveedor.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmEliminarProveedor.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frmEliminarProveedor.getContentPane().setLayout(null);
 		
 		JLabel lblIngreseElId = new JLabel("Ingrese el id del proveedor que desea eliminar");
@@ -57,18 +50,27 @@ public class FormEliminarProveedor {
 		JButton btnEliminar = new JButton("Eliminar");
 		btnEliminar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				Administrador a;
 				try {
-					a = new Administrador("1037657390","juan","1234");
-					a.eliminarProveedor(textField.getText());
+					if (textField.getText()==null){
+						JOptionPane.showMessageDialog(null, "El campo esta vacio");
+					}else{
+					Admin.eliminarProveedor(textField.getText());
+					}
 				} catch (Exception e1) {
 					// TODO Auto-generated catch block
-					e1.printStackTrace();
+					JOptionPane.showMessageDialog(null, e1.getMessage());
 				}
 			}
 		});
 		btnEliminar.setBounds(137, 104, 115, 29);
 		frmEliminarProveedor.getContentPane().add(btnEliminar);
+	}
+	public JFrame getFrmEliminarProveedor() {
+		return frmEliminarProveedor;
+	}
+
+	public void setFrmEliminarProveedor(JFrame frmBuscarProveedor) {
+		this.frmEliminarProveedor = frmBuscarProveedor;
 	}
 
 }
