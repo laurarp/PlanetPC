1078f3455f0406b21c2e133b029ad50acce91a63
switowski143 <alejandro.mery@hotmail.com>
2017-02-27 21:15:26 -0500

    coco loco

--- src/FormRegistrarPedido.java
+++ src/FormRegistrarPedido.java
@@ -7,6 +7,8 @@ import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Font;
+import java.awt.Color;
 
 public class FormRegistrarPedido {
 
@@ -17,6 +19,7 @@ public class FormRegistrarPedido {
 	private JTextField textField_3;
 	private JButton btnRegistrar;
 	private JefeBodega jefazo;
+	private JLabel lblRegistrarPedido;
 
 	/**
 	 * Create the application.
@@ -37,44 +40,49 @@ public class FormRegistrarPedido {
 	 */
 	private void initialize() {
 		frmRegistrarCompra = new JFrame();
-		frmRegistrarCompra.setTitle("Registrar pedido");
-		frmRegistrarCompra.setBounds(100, 100, 521, 330);
+		frmRegistrarCompra.getContentPane().setBackground(Color.WHITE);
+		frmRegistrarCompra.getContentPane().setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		frmRegistrarCompra.setBounds(100, 100, 521, 367);
 		frmRegistrarCompra.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frmRegistrarCompra.getContentPane().setLayout(null);
 		
 		JLabel lblDescripcionProducto = new JLabel("Descripcion producto");
-		lblDescripcionProducto.setBounds(15, 16, 156, 20);
+		lblDescripcionProducto.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		lblDescripcionProducto.setBounds(15, 79, 156, 20);
 		frmRegistrarCompra.getContentPane().add(lblDescripcionProducto);
 		
 		textField = new JTextField();
-		textField.setBounds(15, 52, 464, 57);
+		textField.setBounds(15, 115, 464, 57);
 		frmRegistrarCompra.getContentPane().add(textField);
 		textField.setColumns(10);
 		
 		JLabel lblIdProveedor = new JLabel("Id proveedor");
-		lblIdProveedor.setBounds(15, 125, 118, 20);
+		lblIdProveedor.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		lblIdProveedor.setBounds(15, 188, 118, 20);
 		frmRegistrarCompra.getContentPane().add(lblIdProveedor);
 		
 		textField_1 = new JTextField();
-		textField_1.setBounds(15, 154, 146, 26);
+		textField_1.setBounds(15, 224, 146, 26);
 		frmRegistrarCompra.getContentPane().add(textField_1);
 		textField_1.setColumns(10);
 		
 		JLabel lblCantidad = new JLabel("Cantidad");
-		lblCantidad.setBounds(204, 125, 84, 20);
+		lblCantidad.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		lblCantidad.setBounds(204, 188, 84, 20);
 		frmRegistrarCompra.getContentPane().add(lblCantidad);
 		
 		textField_2 = new JTextField();
-		textField_2.setBounds(204, 154, 84, 26);
+		textField_2.setBounds(204, 224, 84, 26);
 		frmRegistrarCompra.getContentPane().add(textField_2);
 		textField_2.setColumns(10);
 		
 		JLabel lblPrecioCompra = new JLabel("Precio compra");
-		lblPrecioCompra.setBounds(333, 125, 139, 20);
+		lblPrecioCompra.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		lblPrecioCompra.setBounds(340, 188, 139, 20);
 		frmRegistrarCompra.getContentPane().add(lblPrecioCompra);
 		
 		textField_3 = new JTextField();
-		textField_3.setBounds(333, 154, 146, 26);
+		textField_3.setBounds(333, 224, 146, 26);
 		frmRegistrarCompra.getContentPane().add(textField_3);
 		textField_3.setColumns(10);
 		
@@ -93,8 +101,13 @@ public class FormRegistrarPedido {
 				}
 			}
 		});
-		btnRegistrar.setBounds(187, 213, 115, 29);
+		btnRegistrar.setBounds(189, 266, 115, 29);
 		frmRegistrarCompra.getContentPane().add(btnRegistrar);
+		
+		lblRegistrarPedido = new JLabel("Registrar pedido");
+		lblRegistrarPedido.setFont(new Font("Segoe UI", Font.BOLD, 24));
+		lblRegistrarPedido.setBounds(15, 16, 273, 47);
+		frmRegistrarCompra.getContentPane().add(lblRegistrarPedido);
 	}
 	
 	public JFrame getFrmRegistrarPedido() {
