c479c466a4755a02ce94aad2e1d69687b11749b7
CristianGiraldo1309 <cristiand47giraldo@hotmail.com>
2017-02-27 20:33:39 -0500

    act 3

--- src/FormUbicarProducto.java
+++ src/FormUbicarProducto.java
@@ -4,12 +4,18 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.awt.event.ActionEvent;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
 public class FormUbicarProducto extends JFrame {
 
@@ -18,6 +24,7 @@ public class FormUbicarProducto extends JFrame {
 	private JTextField textCantidad;
 	private JTextField textUbicacion;
 	private AuxiliarAlmacenamiento auxiliar;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -40,7 +47,7 @@ public class FormUbicarProducto extends JFrame {
 	 */
 	public FormUbicarProducto() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 449, 189);
+		setBounds(100, 100, 559, 549);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -86,6 +93,22 @@ public class FormUbicarProducto extends JFrame {
 		JButton btnNewButton = new JButton("Ubicar");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				int c = 0;
+				boolean intentar = true;
+				boolean intentar2 = true;
+				try{
+					c = Integer.parseInt(textCantidad.getText());
+					intentar = true;
+				}catch (NumberFormatException e){
+					  intentar = false;
+					  JOptionPane.showMessageDialog(contentPane, "Cantidad debe ser un numero entero");
+					  textCantidad.setText("");
+				}
+				if (textId.getText().isEmpty()){
+					intentar2 = false;
+					JOptionPane.showMessageDialog(contentPane, "No ha ingresado el id del producto para ubicar");
+				}
+				if (intentar == true & intentar2==true){
 				try {
 					auxiliar.ubicarProducto(textId.getText(), Integer.parseInt(textCantidad.getText()), textUbicacion.getText());
 					textId.setText("");
@@ -98,14 +121,59 @@ public class FormUbicarProducto extends JFrame {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
+				}
 				
 			}
 		});
-		btnNewButton.setBounds(72, 105, 89, 23);
+		btnNewButton.setBounds(71, 108, 89, 23);
 		contentPane.add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("Cerrar");
-		btnNewButton_1.setBounds(287, 105, 89, 23);
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+				dispose();
+			}
+		});
+		btnNewButton_1.setBounds(446, 477, 89, 23);
 		contentPane.add(btnNewButton_1);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 142, 426, 358);
+		contentPane.add(scrollPane);
+		String titulos[] = { "Id ", "Precio", "Tipo", "Garantia", "Marca",
+		"Modelo" };
+		DefaultTableModel tableModel = new DefaultTableModel(titulos, 0);
+		table = new JTable(tableModel);
+		scrollPane.setViewportView(table);
+		ArrayList<DescripcionProducto> catalogo = null;
+
+		int rowCount = table.getRowCount();
+
+		for (int i = rowCount - 1; i >= 0; i--) {
+			tableModel.removeRow(i);
+		}
+
+		try {
+			catalogo = auxiliar.mostrarCatalogo();
+		} catch (Exception e1) {
+			JOptionPane.showMessageDialog(null, e1.getMessage());
+		}
+
+		if (catalogo != null) {
+			for (int i = 0; i < catalogo.size(); i++) {
+				String id = catalogo.get(i).getId();
+				String precioVenta = String.valueOf(catalogo.get(i).getPrecioVenta());
+				String tipo = catalogo.get(i).getTipo();
+				String garantia = String.valueOf(catalogo.get(i).getDiasGarantia());
+				String marca = catalogo.get(i).getMarca();
+				String modelo = String.valueOf(catalogo.get(i).getModelo());
+				
+				Object[] objs = { id, precioVenta, tipo, garantia, marca, modelo };
+				tableModel.addRow(objs);
+			}
+		} else {
+			JOptionPane.showMessageDialog(null, "no hay productos en el catalogo");
+		}
 	}
 }
